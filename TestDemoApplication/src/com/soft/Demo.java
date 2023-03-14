9eea1b41a359a6e2f91bbbd48660abffb43764f2
saylichopade28 <saylichopade28@gmail.com>
2023-03-14 15:57:46 +0530

    changes in main method

--- TestDemoApplication/src/com/soft/Demo.java
+++ TestDemoApplication/src/com/soft/Demo.java
@@ -3,8 +3,8 @@ package com.soft;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hello world!");
+		System.out.println("hello world welcome!");
 	}
 
 }
